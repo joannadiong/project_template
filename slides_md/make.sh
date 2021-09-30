0938f89155938731eb442845124b3ad5590fe142
Joanna Diong <joanna.diong@gmail.com>
2021-09-30 14:10:03 +1000

    Update slides make.sh

--- slides_md/make.sh
+++ slides_md/make.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 # Synaptics install pandoc, texlive-xetex
-pandoc slides.md \ 
---pdf-engine=xelatex \ 
---citeproc \ 
---wrap=preserve \ 
---bibliography=ref.bib \ 
---dpi=300 \ 
-from=markdown+escaped_line_breaks \ 
--t beamer -o slides.pdf
+pandoc slides.md --pdf-engine=xelatex --citeproc --wrap=preserve --bibliography=ref.bib --dpi=300 -t beamer -o slides.pdf
